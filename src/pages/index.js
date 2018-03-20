9b67a63d9220d1fc95c75ad6f8fec58350d1a8ba
wholeNaNyards <wholenanyards@gmail.com>
2018-03-20 00:05:00 -0700

    Initial work on blog index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-);
+const IndexPage = ({ data }) => {
+  const { allMarkdownRemark: { edges } } = data;
+  return (
+    <div>
+      <h1>Hi people</h1>
+      <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      <Link to="/page-2/">Go to page 2</Link>
+      <h2>Index</h2>
+      {edges.map(({ node: { frontmatter: post } }) => (
+        <div>
+          <Link to={post.path}>{post.title}</Link>
+          <br />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export const pageQuery = graphql`
+  query IndexQuery {
+    allMarkdownRemark(limit: 10) {
+      edges {
+        node {
+          frontmatter {
+            title
+            path
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
