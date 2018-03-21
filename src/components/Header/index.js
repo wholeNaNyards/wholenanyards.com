870ef9f3eb061f1ca6cd7c6894ce049398ba4ff7
wholeNaNyards <wholenanyards@gmail.com>
2018-03-21 01:56:23 -0700

    Added styled components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,28 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import styled from 'styled-components';
 
 import logo from '../../images/kylo.jpg';
 
+const HeaderWrapper = styled.div`
+  background: #524763;
+  margin-bottom: 1.45rem;
+  h1 {
+    img {
+      height: 150px;
+    }
+  }
+`;
+
+const HeaderContainer = styled.div`
+  margin: 0 auto;
+  max-width: 960;
+  padding: 1.45rem 1.0875rem;
+`;
+
 const Header = () => (
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
+  <HeaderWrapper>
+    <HeaderContainer>
       <h1 style={{ margin: 0 }}>
         <Link
           to="/"
@@ -28,8 +34,8 @@ const Header = () => (
           <img src={logo} alt="Kyle Ren" />
         </Link>
       </h1>
-    </div>
-  </div>
+    </HeaderContainer>
+  </HeaderWrapper>
 );
 
 export default Header;
