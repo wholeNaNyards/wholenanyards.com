ebd76bd9ab91c75655bf41dfbfdf1dfe54e3f213
wholeNaNyards <wholenanyards@gmail.com>
2018-03-21 01:20:22 -0700

    Added static assets

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Link from 'gatsby-link';
 
+import logo from '../../images/kylo.jpg';
+
 const Header = () => (
   <div
     style={{
@@ -23,7 +25,7 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          <img src={logo} alt="Kyle Ren" />
         </Link>
       </h1>
     </div>
